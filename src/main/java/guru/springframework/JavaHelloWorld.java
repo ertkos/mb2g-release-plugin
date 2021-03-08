e3d35f1efff4a89cbd7f81df28f5fc2470ba1746
ert112233 <ertugrlkose@gmail.com>
2021-03-08 12:14:50 +0300

    some things

--- src/main/java/guru/springframework/JavaHelloWorld.java
+++ src/main/java/guru/springframework/JavaHelloWorld.java
@@ -1,11 +1,19 @@
 package guru.springframework;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by jt on 2018-12-09.
  */
 public class JavaHelloWorld {
-
+    private static final Logger logger = LoggerFactory.getLogger(JavaHelloWorld.class);
     public String getHello(){
+        logger.debug("debug level logudur");
+        logger.info("info level logudur");
+        logger.warn("warn level logudur");
+        logger.error("error level logudur");
         return "Hello World";
     }
 
--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -2,10 +2,25 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 class JavaHelloWorldTest {
 
     @Test
-    void getHello() {
+    void getHello() throws InterruptedException {
+
+        LocalDate localDate = LocalDate.now().minusMonths(1L);
+        System.out.println(localDate);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        LocalTime localTime = LocalTime.now();
+        Thread.sleep(2000L);
+        LocalTime localTime2 = LocalTime.now();
+        System.out.println(localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        System.out.println(localTime2.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         System.out.println(new JavaHelloWorld().getHello());
 
     }
